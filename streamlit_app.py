893ebdbbfd524d223b28b3592a9596e4b235fde6
Yadnesh Ganesh Takalkar <66586860+yadnesh1998@users.noreply.github.com>
2023-08-04 14:10:25 +0530

    Update streamlit_app.py
    created new header for own fruit smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.header('🥣 Breakfast Menu')
 streamlit.text('🥗 Omega 3 and Blueberry Oatmeal')
 streamlit.text('🐔 Kale, Spinach and Rocket Smoothie')
 streamlit.text('🥑🍞 Hard Boiled Free Ranged Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
